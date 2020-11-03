dda2433c6811bd52ae2b6a73d43923c760435239
DeCortez <decorte.daniel@hotmail.cz>
2020-11-03 16:53:23 +0100

    Create park_info.clj
    Storing all data about a park(Bertramka) in a variable as strings. Simple solution that came to mind first.

---
+++ park_info.clj
@@ -0,0 +1,31 @@
+(
+    def Bertramka{
+        ;---Basic Info---
+        :food "n/a"
+        :food_extra "n/a"
+        :toilet "yes"
+        :toilet_extra "n/a"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "cultural monument"
+        :POI2 "classical Music Concerts"
+        :POI3 "social events"
+        :POI4 "W. A. Mozart museum"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "no"
+        :sports_field "no"
+        :playground "no"
+        ;---Directions---
+        :public_transport "Trams #4, #7, #9, #10, #58, #59"
+        :GPS_coords "50°4’15.00’’ N, 14°23’42.00’’ E"
+        :parking "yes"
+        :parking_extra "OC Smíchov underground parking"
+        ;---Additional Information---
+        :path_type "paved"
+        :administered_by1 "Prague 5"
+        :administered_by2 "Comenius company"
+        :opening_hours "April-October: 09:00-18:00, November-March: 09:30-16:00"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/fe/bd/6a/184189_4_bertramka_mapa.pdf"
+    }
+)
