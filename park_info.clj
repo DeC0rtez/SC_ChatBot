dfe90cc7bd1fd89ef66fe254015606926fbff52e
DeCortez <decorte.daniel@hotmail.cz>
2020-11-25 13:06:05 +0100

    Update park_info.clj
    Added Obora Hvězda, Františkánská Zahrada

--- park_info.clj
+++ park_info.clj
@@ -28,4 +28,101 @@
         ;---Map---
         :map_url "https://www.praha.eu/public/fe/bd/6a/184189_4_bertramka_mapa.pdf"
     }
+    def Frantiskanska_Zahrada{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "In the vicinity - Alfa passage, Wenceslaus & Jungmann square, Narodni Trida"
+        :toilet "yes"
+        :toilet_extra "with wheelchair access"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Enjoyable dining in the centre of town"
+        :POI2 "Suitable for children and the elderly"
+        :POI3 "Dogs not allowed"
+        :POI4 "Church of Our Lady of the Snows"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "no"
+        :sports_field "no"
+        :playground "yes"
+        ;---Directions---
+        :public_transport "Wenceslaus Sq. Trams #3, #9, #14, #24, #51, #52, #54, #55, #56, #58, metro lines: A, B station: Můstek"
+        :GPS_coords "50°4′58.8″ N, 14°25′24.8″ E"
+        :parking "no"
+        :parking_extra "n/a"
+        ;---Additional Information---
+        :path_type "paved, special surface in child playpen"
+        :administered_by1 "Prague City"
+        :administered_by2 "n/a"
+        :opening_hours "Year long, closes at night"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/50/c9/77/184207_4_frantiskanska_mapa.pdf"
+       
+    }
+    def Obora_Hvezda{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "During Summer Season on the premises of the Villa (www.pamatniknarodnihopisemnictvi.cz), tel: 235 357 938,
+        or at Libocká street, restaurant U Holečků, www.uholecku.cz,
+        or at Libocká studna, tel.: 420 777 836 966,
+        alternatively at Petřiny"
+        :toilet "yes"
+        :toilet_extra "Portable toilets on the premises, during summer season toilets are also available in the villa"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Suitable for families with children"
+        :POI2 "Suitable for tourists"
+        :POI3 "Dogs are allowed"
+        :POI4 "Memorable Trees"
+        :POI5 "Nature trail"
+        :POI6 "Natural sights"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "no"
+        :sports_field "no, nearby: Youth House (www.ddmp6.cz/kontakt/) & traffic playground"
+        :playground "yes, 2 playgrounds"
+        ;---Directions---
+        :public_transport "
+        stop: Nad Markétou - buses #179, #184, #191, #510, 
+        stop: Libocká - buses #179, #510, 
+        stop: Vypich - trams #15, #22, #25, #57"
+        :GPS_coords "50°5'2.798" N, 14°19'45.529" E"
+        :parking "yes"
+        :parking_extra "8 parking spots at Libocká gate, free parking in U Hvězdy street"
+        ;---Additional Information---
+        :path_type "Hardened soil, threshing floor"
+        :administered_by1 "Museum of Czech Literature"
+        :administered_by2 "Prague City"
+        :opening_hours "park - year long, villa - (April 10:00-17:00, May-September 10:00-18:00, October 10:00-17:00, November-March CLOSED) "
+        ;---Map---
+        :map_url "https://www.praha.eu/public/bf/26/3e/184361_4_hvezda_mapa.pdf"       
+    }
+    def Kampa{
+       
+    }
+    def Kinskeho_zahrada{
+       
+    }
+    def Klamovka{
+       
+    }
+    def Ladronka{
+       
+    }
+    def Letna{
+       
+    }
+    def Petrin{
+       
+    }
+    def Riegrovy_sady{
+       
+    }
+    def Stromovka{
+       
+    }
+    def Vojanovy_sady{
+       
+    }
+    def Vysehrad{
+       
+    }
 )
