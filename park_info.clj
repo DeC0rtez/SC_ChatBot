56ae30eee60b6624224e876081505c67cc099815
DeCortez <decorte.daniel@hotmail.cz>
2020-11-27 04:03:33 +0100

    Update park_info.clj
    Parks added: Kampa, Kinskeho Zahrada, Klamovka, Ladronka, Letná and Petřín. 4 left till completion

--- park_info.clj
+++ park_info.clj
@@ -52,7 +52,6 @@
         ;---Additional Information---
         :path_type "paved, special surface in child playpen"
         :administered_by1 "Prague City"
-        :administered_by2 "n/a"
         :opening_hours "Year long, closes at night"
         ;---Map---
         :map_url "https://www.praha.eu/public/50/c9/77/184207_4_frantiskanska_mapa.pdf"
@@ -96,21 +95,205 @@
         :map_url "https://www.praha.eu/public/bf/26/3e/184361_4_hvezda_mapa.pdf"       
     }
     def Kampa{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "restaurant: Rybí Restaurace,
+        café: Mlýnská (tel. 608 444 490)
+        restaurant/café : Sovovy Mlýny"
+        :toilet "yes"
+        :toilet_extra "n/a"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "View of the river"
+        :POI2 "Sovovy Mlýny gallery"
+        :POI3 "Čertovka"
+        :POI4 "Charles Bridge"
+        :POI5 "Werich House"
+        :POI6 "Memorable Trees"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "yes"
+        :sports_field "no, grass-covered segments accessible"
+        :playground "yes, in the vicinity: Community and Maternal centre Kampa"
+        ;---Directions---
+        :public_transport "
+        stop: Helichova - trams #12, #20, #20, #22, #23, #57 
+        stop: Újezd - trams #6, #9, #22, #23, #57, #58, #59"
+        :GPS_coords "50°5'16.87" N, 14°24'14.58" E"
+        :parking "no"
+        :parking_extra "n/a"
+        ;---Additional Information---
+        :path_type "asphalt"
+        :administered_by1 "Municipality Prague 1"
+        :administered_by2 "Prague City"
+        :opening_hours "Open year-round"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/7c/a6/ed/184308_4_kampa_mapa.pdf"       
        
     }
     def Kinskeho_zahrada{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "café in Musaion museum of natural sciences"
+        :toilet "yes"
+        :toilet_extra "n/a"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Enjoyable location for a walk with children"
+        :POI2 "Quiet and relaxing space in the centre of town"
+        :POI3 "Musaion Museum of Natural Sciences"
+        :POI4 "Decorative Ponds"
+        :POI5 "Natural attraction - Petřín rock formations"
+        ;---Children and Sport---
+        :bike_path "no"
+        :skating "no"
+        :sports_field "no"
+        :playground "yes, by the Kinských square entrance, nearby the Kinských Villa and more under construction"
+        ;---Directions---
+        :public_transport "
+        stop: Kobrova - bus #176"
+        :GPS_coords "50° 4’ 44.26’’ N, 14° 24’ 0.21’’ N"
+        :parking "yes"
+        :parking_extra "OC Smíchov underground parking"
+        ;---Additional Information---
+        :path_type "paved"
+        :administered_by1 "Prague City"
+        :opening_hours "Open year-round"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/3/95/58/184325_4_kinskeho_mapa.pdf"       
        
     }
     def Klamovka{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "restaurant & club Klamovka"
+        :toilet "yes"
+        :toilet_extra "in the restaurant and club"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Historical sights"
+        :POI2 "Art gallery"
+        :POI3 "Musical club with restaurant in garden"
+        :POI4 "playground for small children"
+        :POI5 "Dogs permitted"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "no"
+        :sports_field "Tennis courts and sports facilities in Sokolovna gymnasium"
+        :playground "yes, by the garden restaurant"
+        ;---Directions---
+        :public_transport "
+        stop: Klamovka - buses #149, #191, #217, - trams #4, #7, #9, #10, #58, #59"
+        :GPS_coords "50°4'17,04" N, 14°22'39,72" E"
+        :parking "no"
+        :parking_extra "n/a"
+        ;---Additional Information---
+        :path_type "paved & asphalt"
+        :administered_by1 "Municipality Prague 5"
+        :opening_hours "Open year-round"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/16/84/48/184296_4_klamovka_mapa.pdf"       
        
     }
     def Ladronka{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "restaurant Ladronka"
+        :toilet "yes"
+        :toilet_extra "in the restaurant"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Large area suited for free time activities and relaxation for all age categories"
+        :POI2 "dogs permitted"
+        :POI3 "during the year: musical performances and social events"
+        :POI4 "suited for a day-long stay"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "yes"
+        :sports_field "No, nearby: traffic playfround and Vypich Youth House"
+        :playground "yes, by the restaurant and by the Ladronka bus stop"
+        ;---Directions---
+        :public_transport "
+        stop: U ladronky/ Rozýnova/ Štefkova - bus #191
+        stop: Vypich - buses #108, #174, #180, #191, #I3, - trams: #15, 22, 25, 57"
+        :GPS_coords "50°4'47.447" N, 14°21'52.445" E"
+        :parking "yes"
+        :parking_extra "Yes, in streets Tomanova & Skokanská"
+        ;---Additional Information---
+        :path_type "paved, threshing & asphalt"
+        :administered_by1 "Municipality Prague 6"
+        :opening_hours "Open year-round"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/72/f3/b/184218_4_ladronka_mapa.pdf"       
        
     }
     def Letna{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "Letenský estate (www.letenskyzamecek.cz), Hanavský pavilion (www.hanavskypavilon.cz) "
+        :toilet "yes"
+        :toilet_extra "in the restaurants"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Spacious area for recreation, suited for all age categories"
+        :POI2 "dogs permitted"
+        :POI3 "during the year: cultural events"
+        :POI4 "suited for a day-long stay"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "yes"
+        :sports_field "No, however sports and recreational activities are permitted on grassy segments,
+        2x skates rentals: Rental #1 - Skala sport tel: +420 605 258 670, Rental #2 - tel: +420 220 571 721
+        General purpose rental: AC Sparta rental - tel: +420 603 938 328,
+        small pitch for football, paid access tennis courts on premises of LTC Slavia - tel: +420 777 789 140"
+        :playground "yes, 4 in total. By the letenský estate, 2 by the tennis courts, one by Hanavský pavillion"
+        ;---Directions---
+        :public_transport "
+        stop: Letenské Náměstí/ Sparta - trams: #1, #8, #15, #25, #26, #51, #56
+        stop: Čechův most - trams: #12, #17, #53
+        stop: Chotkovy Sady - trams: #18, #20, #57
+        stop: Hradčanská metro - metro line A"
+        :GPS_coords "50°5'41" N, 14°24'57" E"
+        :parking "yes"
+        :parking_extra "Yes, in Kostelní street"
+        ;---Additional Information---
+        :path_type "asphalt"
+        :administered_by1 "Prague City"
+        :opening_hours "Open year-round"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/e3/49/62/184240_4_letna_mapa.pdf"      
        
     }
     def Petrin{
+        ;---Basic Info---
+        :food "yes"
+        :food_extra "Restaurants: Nebozízek, Petřínské terasy, Outlook Tower buffet, Peklo, Bella Vista"
+        :toilet "yes"
+        :toilet_extra "in restaurants and by outlook tower"
+        :POIs "yes"  ;POI refers to Point of Interest
+        :POI1 "Suitable for families with children and tourists"
+        :POI2 "accessible on foot or by cablecar"
+        :POI3 "Outlook tower"
+        :POI4 "Mirror maze"
+        :POI5 "Astronomy tower"
+        :POI6 "Mineshafts"
+        :POI7 "Fruit orchards"
+        ;---Children and Sport---
+        :bike_path "yes"
+        :skating "yes"
+        :sports_field "no"
+        :playground "yes, by Hellichova street"
+        ;---Directions---
+        :public_transport "
+        stop: Újezd - trams: #6, #9, #12, #20, #22, #23, #57, #58, #59
+        stop: Hellichova - trams: #12, #20, #22, #23, #57
+        stop: Pohořelec - trams: #22, #23
+        stop: Koleje Strahov - buses: #143, #149, #217"
+        :GPS_coords "50°05′2.93″ N, 14°23′40.88″ E"
+        :parking "yes"
+        :parking_extra "by Strahov stadium"
+        ;---Additional Information---
+        :path_type "asphalt, concrete, paved, threshing"
+        :administered_by1 "Prague City"
+        :opening_hours "Petřín & Astronomy Station: open year-round
+        Outlook tower: January-March, November - (10:00-18:00), April, September, October - (10:00-19:00), May-August - (10:00-22:00)"
+        ;---Map---
+        :map_url "https://www.praha.eu/public/d/77/d/184279_4_petrin_mapa.pdf"      
        
     }
     def Riegrovy_sady{
