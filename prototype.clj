b95a58c4b109f32b8be470f6c76c202867430ff4
DeCortez <decorte.daniel@hotmail.cz>
2020-12-05 01:37:20 +0100

    Update prototype.clj
    Added a function, assign_keyword_category which assigns a keyword to a category. This function is used in the loop and prints a response based on the assigned category,

--- prototype.clj
+++ prototype.clj
@@ -1,5 +1,34 @@
 (require '[clojure.string :as string])
 
+(def Bertramka{
+    ;---Basic Info---
+    :food nil
+    :food_extra nil
+    :toilet true
+    :toilet_extra nil
+    :POIs "Cultural monument, classical music concerts, social events, W.A. Mozart Museum" ;POI refers to Point of Interest
+    ;---Children and Sport---
+    :bike_path true
+    :skating false
+    :sports_field false
+    :sports_field_extra nil
+    :playground false
+    :playground_extra nil
+    ;---Directions---
+    :public_transport "
+    stop: Bertramka - trams: #4, #7, #9, #10, #58, #59"
+    :GPS_coords "50°4′15.00″ N, 14°23′42.00″ E"
+    :parking true
+    :parking_extra "OC Smíchov underground parking"
+    ;---Additional Information---
+    :dogs nil
+    :path_type "paved"
+    :administered_by "Prague 5 & Comenius Company"
+    :opening_hours "April-October: 09:00-18:00, November-March: 09:30-16:00"
+    ;---Map---
+    :map_url "https://www.praha.eu/public/fe/bd/6a/184189_4_bertramka_mapa.pdf"
+})
+
 (def phrases { ;example phrases
         :dogs {
             :p1 "Yes, do not hesitate to bring your four-legged friend with you."
@@ -9,6 +38,17 @@
             :p1 "Dining is available at this park."
             :p2 "Don't worry, you will not go hungry at this park!"
             :p3 "Don't worry, you will not go hungry at this park!"}})
+
+(def synonyms_dogs #{"Dog", "dog", "doggo", "doggie", "pet"})
+(def synonyms_food #{"eat", "dinner", "lunch", "breakfast", "food", "dine", "diner", "hunger", "hungry", "snack", "meal", "meals", "drink", "drinkings", "drinks"})
+
+(defn assign_keyword_category 
+[keyword]
+  (cond
+    (contains? synonyms_dogs keyword) (def category :dogs)
+    (contains? synonyms_food keyword) (def category :food)
+    :else (println "This is an incorrect input"))
+  (println "Category is: " category))
 
 (defn get_response [p_cat] ;get response function that fetches a random response based on keyword
     (def rn (rand-int 4))
@@ -26,9 +66,10 @@
          (println "Thank you and see you next time!")) ;prints message if user input is "stop", no more recursion, ends the loop
       (do
         (println "You typed: " input)
+        (assign_keyword_category input)
         (cond
-           (= input "dogs") (get_response :dogs)
-           (= input "food") (get_response :food)
+           (= category :dogs) (get_response :dogs)
+           (= category :food) (get_response :food)
            :else (println "This is an incorrect input"))
         (recur (read-line))) ;repeats loop aka recursion, takes new input, repeats process
       )
